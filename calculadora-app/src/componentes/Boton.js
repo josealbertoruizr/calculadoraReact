a7cbb6e64156764474f72953f2920805f7cc0175
josealbertoruizr <josealbertoruizr@gmail.com>
2023-12-13 19:46:31 -0600

    Update Boton.js

--- calculadora-app/src/componentes/Boton.js
+++ calculadora-app/src/componentes/Boton.js
@@ -4,7 +4,7 @@ import '../stylesheet/Boton.css'
 function Boton(props) {
 
     const esOperador = (valor) => {
-        return isNaN(valor) && (valor != '.') && (valor != '=');
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
     }
 
     return (
