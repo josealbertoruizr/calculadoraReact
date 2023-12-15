e7ca80a34e70d6fa385fa11c774b49866649e1f9
josealbertoruizr <josealbertoruizr@gmail.com>
2023-12-14 18:12:32 -0600

    Update BotonClear.js

--- calculadora-app/src/componentes/BotonClear.js
+++ calculadora-app/src/componentes/BotonClear.js
@@ -2,7 +2,8 @@ import React from 'react';
 import '../stylesheet/BotonClear.css'
 
 const BotonClear = (props) => (
-    <div className='boton-clear'
+    <div
+        className='boton-clear'
         onClick={props.manageClear}>
         {props.children}
     </div>
